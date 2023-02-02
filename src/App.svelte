99422c023377077914f6536a580e30d0cbde12d7
tpuddu <63052802+tpuddu@users.noreply.github.com>
2023-02-02 19:38:55 +0100

    Add meta tags for set improvement

--- src/App.svelte
+++ src/App.svelte
@@ -19,6 +19,31 @@
 	}
 </script>
 
+<svelte:head>
+	<title>TH Köln Recommender System</title>
+	<!-- meta -->
+	<!-- general -->
+	<meta name="description" content="Expertensuche und Empfehlung auf Basis von Fach- und Themengebieten an der TH Köln.">
+	<meta name=”robots” content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<!-- Google -->
+	<meta name="google-site-verification" content="TODO: site verification key here"> <!-- todo -->
+	<!-- OGP (Facebook, Xing, LinkedIn, ...) -->
+	<meta property="og:title" content="TH Köln Recommender System: Expertensuche an der TH Köln" />
+	<meta property="og:image" content="TODO_IMAGE_URL" /> <!-- todo min 600x314, ratio 1,91:1 -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="TODO_URL_OF_THIS_WEBSITE" /> <!-- todo -->
+	<meta property="og:site_name" content="TH Köln" />
+	<meta property=“og:description“ content="Expertensuche und Empfehlung auf Basis von Fach- und Themengebieten an der TH Köln." />
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary_large_image" /> <!-- summary or summary_large_image is recommended for websites -->
+	<meta name="twitter:title" content="TH Köln Recommender System: Expertensuche an der TH Köln" />
+	<meta name="twitter:description" content="Expertensuche und Empfehlung auf Basis von Fach- und Themengebieten an der TH Köln." />
+	<meta name="twitter:image" content="TODO_IMAGE_URL" /> <!-- todo recommended for summary_large_image: 800x419 or 1600x838 (ratio: 1,91:1) -->
+	<meta name="twitter:site" content="@th_koeln" />
+</svelte:head>
+
 <body>
 	<Router {routes}/>
 </body>
